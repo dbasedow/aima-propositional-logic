9d2ecc3076442262f7a2215701f3baadffbd9147
Daniel Basedow <daniel.basedow@gmail.com>
2014-09-11 20:46:43 +0200

    remove agent

--- aima-core/src/test/java/aima/test/core/unit/AllAIMAUnitTestSuite.java
+++ aima-core/src/test/java/aima/test/core/unit/AllAIMAUnitTestSuite.java
@@ -3,12 +3,11 @@ package aima.test.core.unit;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import aima.test.core.unit.agent.AgentTestSuite;
 import aima.test.core.unit.logic.LogicTestSuite;
 import aima.test.core.unit.util.UtilTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AgentTestSuite.class,
+@Suite.SuiteClasses({
 		LogicTestSuite.class,
 		UtilTestSuite.class })
 public class AllAIMAUnitTestSuite {
