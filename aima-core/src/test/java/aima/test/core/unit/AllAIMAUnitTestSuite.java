e5f933244e8b46f417cecfd3a900a770697f548b
Daniel Basedow <daniel.basedow@gmail.com>
2014-09-11 12:44:10 +0200

    remove environment, probability and search packages

--- aima-core/src/test/java/aima/test/core/unit/AllAIMAUnitTestSuite.java
+++ aima-core/src/test/java/aima/test/core/unit/AllAIMAUnitTestSuite.java
@@ -4,15 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import aima.test.core.unit.agent.AgentTestSuite;
-import aima.test.core.unit.environment.EnvironmentTestSuite;
 import aima.test.core.unit.logic.LogicTestSuite;
-import aima.test.core.unit.probability.ProbabilityTestSuite;
-import aima.test.core.unit.search.SearchTestSuite;
 import aima.test.core.unit.util.UtilTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AgentTestSuite.class, EnvironmentTestSuite.class,
+@Suite.SuiteClasses({ AgentTestSuite.class,
 		LogicTestSuite.class,
-		ProbabilityTestSuite.class, SearchTestSuite.class, UtilTestSuite.class })
+		UtilTestSuite.class })
 public class AllAIMAUnitTestSuite {
 }
