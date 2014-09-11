26bd019dd95ad72ec09a8966cd1cd41ab2a563b1
Daniel Basedow <daniel.basedow@gmail.com>
2014-09-11 19:46:07 +0200

    remove first order logic package (fol)

--- aima-core/src/test/java/aima/test/core/unit/logic/LogicTestSuite.java
+++ aima-core/src/test/java/aima/test/core/unit/logic/LogicTestSuite.java
@@ -3,11 +3,10 @@ package aima.test.core.unit.logic;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import aima.test.core.unit.logic.fol.FOLTestSuite;
 import aima.test.core.unit.logic.propositional.PropositionalTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ FOLTestSuite.class, PropositionalTestSuite.class })
+@Suite.SuiteClasses({PropositionalTestSuite.class})
 public class LogicTestSuite {
 
 }
